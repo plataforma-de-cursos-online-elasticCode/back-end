7a1349bf7be671cedbd45d11af3659477401b86f
Enzo-Jorge-Nascimento <enzojorge23@gmail.com>
2025-06-25 09:21:46 -0300

    fix: Permitindo o entity scan no microserviço de matrícula

--- matricula-service/src/main/java/br/com/solutis/matricula_service/MatriculaServiceApplication.java
+++ matricula-service/src/main/java/br/com/solutis/matricula_service/MatriculaServiceApplication.java
@@ -2,8 +2,22 @@ package br.com.solutis.matricula_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = {
+		"br.com.solutis.matricula_service.entity",
+		"br.com.solutis.usuario_service.entity",
+		"br.com.solutis.usuario_service.entity.usuario",
+		"br.com.solutis.curso_service.entity",
+		"br.com.solutis.curso_service.entity.curso"
+})
+@EnableJpaRepositories(basePackages = {
+		"br.com.solutis.matricula_service.repository",
+		"br.com.solutis.usuario_service.repository",
+		"br.com.solutis.curso_service.repository"
+})
 public class MatriculaServiceApplication {
 
 	public static void main(String[] args) {
